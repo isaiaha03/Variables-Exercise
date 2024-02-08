801058bae58457cccbbe6a32f21130bf352a2c14
isaiaha03 <isaiahalvarez2003@gmail.com>
2024-02-07 21:18:44 -0600

    Completed variable exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dogName = "jayjay";
+            int dogAge = 10;
+            char dogInitial = 'j';
+            bool dogAlive = false;
+            double dogWeight = 13.0;
+            double dogHeight = 1.1;
+
+            Console.WriteLine($"My dog's name is {dogName}, he is {dogAge} years old, his initial is {dogInitial}, he is currently alive: {dogAlive}, he weighs {dogWeight} lbs, and is {dogHeight} feet tall.");
         }
     }
 }
